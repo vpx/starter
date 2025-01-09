5e328e85383f45adb82a769191ddaf2da77f3548
Cristian Milani <cristian.milani@alten.it>
2025-01-09 15:18:37 +0100

    Map <Space>+<Tab> to :ClangdSwitchSourceHeader

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,11 +6,9 @@ local function mark_and_move(motion)
     return function()
         local count = vim.v.count > 0 and vim.v.count or ""
         if count ~= "" then
-            print('a')
             vim.cmd("normal! m'")
             vim.cmd("normal! " .. count .. motion)
         else
-            print('b')
             vim.cmd("normal! " .. motion)
         end
     end
@@ -23,6 +21,10 @@ end
 map('i', '<Tab>', '<Tab>', { noremap = true, silent = true })
 
 map('n', '<Space>k', ':lua vim.lsp.buf.format()<CR>', {
+    noremap = true, silent = true
+})
+
+map('n', '<Space><Tab>', function() vim.cmd('ClangdSwitchSourceHeader') end, {
     noremap = true, silent = true
 })
 
